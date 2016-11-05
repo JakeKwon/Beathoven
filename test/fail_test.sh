ec65f30b4593670a88c7e50ce1be68c9fdf71fd7
Eunice Kokor <eek2138@columbia.edu>
2016-11-05 15:27:50 -0400

    script to compile beathoven files

--- test/fail_test.sh
+++ test/fail_test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+
 NC='\033[0m'
 CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 
-INPUTS="scanner/*.in"
-printf "${CYAN} Running Scanner Tests!${NC}"
+INPUTS="compiler/fail/*.in"
+TMP_FILE=$(mktemp "compiled.XXXXX")
+
+printf "${CYAN} Running Compiler Fail Tests! \n${NC}"
 
 for infile in $INPUTS; do
-    outfile=${infile/.in/.out}
-    scanner/tokenize < $infile | cmp -s $outfile -
+    llvm_file=${infile/.bet/.ll}
+    outfile=${infile/.bet/.out}
+
+    ../beathoven.sh < $infile | cmp -s $outfile -
     if [ "$?" -eq 0]; then
         printf "%-65s ${GREEN}SUCCESS\n${NC}" " - checking $infile..."
     else
