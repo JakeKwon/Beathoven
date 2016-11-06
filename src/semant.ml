ac4ebb4619b5974fadb07cc293d02bfb5f0728a5
RadonX <radon93@hotmail.com>
2016-11-06 15:40:35 -0500

    create semant.ml

---
+++ src/semant.ml
@@ -0,0 +1,19 @@
+open Ast
+
+module StringMap = Map.Make(String)
+
+(*
+  Semantic checking of a program. Returns void if successful,
+  throws an exception if something is wrong.
+*)
+
+
+(* Check each global variable *)
+
+
+(* check for identically named globals, functions, formal
+  arguments, and local variables *)
+
+(* test assignments and function calls *)
+
+(* todo: check function *)
