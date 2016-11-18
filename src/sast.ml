98239cd4befbcf88896b82d3db9063f84fccf821
sonaroy <sonaroy@icloud.com>
2016-11-18 17:25:01 -0500

    sast formatted for beathoven based on dice

--- src/sast.ml
+++ src/sast.ml
@@ -1,12 +1,24 @@
 open Ast
 
 type sexpr =
-		SInt_Lit of int
+	SId of string * datatype
+	| SLitBool of bool
+	| SLitInt of int
+	| SLitDouble of float
+	| SLitStr of string
+	| SNull
+	| SBinop of sexpr * binary_operator * sexpr * datatype
+	| SUniop of unary_operator * sexpr * datatype
+	| SAssign of sexpr * sexpr * datatype
+	| SFuncCall of string * sexpr list (* datatype at end? *)
+	| SNoexpr
+
+	(* 
+	| 	SInt_Lit of int
 	| 	SBoolean_Lit of bool
 	| 	SFloat_Lit of float
 	| 	SString_Lit of string
 	| 	SChar_Lit of char
-	| 	SId of string * datatype
 	| 	SBinop of sexpr * op * sexpr * datatype
 	| 	SAssign of sexpr * sexpr * datatype
 	| 	SNoexpr
@@ -18,10 +30,20 @@ type sexpr =
 	| 	SArrayPrimitive of sexpr list * datatype
 	|  	SUnop of op * sexpr * datatype
 	| 	SNull
-	| 	SDelete of sexpr
+	| 	SDelete of sexpr *)
 
 type sstmt =
-		SBlock of sstmt list
+	SBlock of sstmt list
+	| SExpr of expr * datatype
+	| SIf of sexpr * sstmt * sstmt
+	| SWhile of sexpr * sstmt
+	| SReturn of sexpr * datatype
+	| SBreak
+	| SContinue
+	| SVarDecl of datatype * string * sexpr
+
+(* 
+	SBlock of sstmt list
 	| 	SExpr of sexpr * datatype
 	| 	SReturn of sexpr  * datatype
 	| 	SIf of sexpr * sstmt * sstmt
@@ -29,10 +51,17 @@ type sstmt =
 	| 	SWhile of sexpr * sstmt
 	|  	SBreak
 	|   SContinue
-	|   SLocal of datatype * string * sexpr
+	|   SLocal of datatype * string * sexpr *)
 
-type func_type = User | Reserved
+(* corresponds to ast lines 46-60*)
+type sfunc_decl = {
+	sfname : string;
+	sformals : bind list;
+	sreturnType : datatype;
+	sbody : sstmt list;
+}
 
+(* 
 type sfunc_decl = {
 	sfname : fname;
 	sreturnType : datatype;
@@ -40,18 +69,4 @@ type sfunc_decl = {
 	sbody : sstmt list;
 	func_type : func_type;
 	source : string;
-}
-
-type sclass_decl = {
-	scname : string;
-	sfields : field list;
-	sfuncs: sfunc_decl list;
-}
-
-(* Class Declarations | All method declarations | Main entry method *)
-type sprogram =  {
-	classes : sclass_decl list;
-	functions : sfunc_decl list;
-	main : sfunc_decl;
-	reserved : sfunc_decl list;
-}
+} *)
